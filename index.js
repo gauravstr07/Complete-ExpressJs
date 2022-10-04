9f095268880a98f378dd3f5f1113a4cf6596418d
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-05 01:23:09 +0530

    Using Middleware by Route

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require("express");
-const reqFilter = require('./middleware');
+const reqFilter = require("./middleware");
 const app = express();
+const route = express.Router();
 
+app.use("/", route);
 app.use(express.json());
-
+route.use(reqFilter);
 const port = 5000;
-
-
 
 app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-app.get("/user", reqFilter, (req, res) => {
+app.get("*", reqFilter, (req, res) => {
+  res.send("Page not found");
+});
+
+route.get("/user", reqFilter, (req, res) => {
   res.send("Welcome💦💞");
 });
 
-app.get("*", reqFilter, (req, res) => {
-  res.send("Page not found");
+route.get("/contact", reqFilter, (req, res) => {
+  res.send("Contact me🔱🛂");
 });
 
 app.listen(port, () => {
